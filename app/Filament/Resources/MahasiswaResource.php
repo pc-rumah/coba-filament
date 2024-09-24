77f21dc4080f26b62960abb4fe275503ccc8f94c
pc-rumah <wow23787@gmail.com>
2024-09-24 18:42:21 +0700

    coba filament

---
+++ app/Filament/Resources/MahasiswaResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\Mahasiswa;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\MahasiswaResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\MahasiswaResource\RelationManagers;
+
+class MahasiswaResource extends Resource
+{
+    protected static ?string $model = Mahasiswa::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        TextInput::make('nim')->required()->unique(ignorable: fn($record) => $record),
+                        TextInput::make('nama')->required(),
+                        TextInput::make('email')->required(),
+                        Select::make('Prodi')->options([
+                            'Teknik Informatika' => 'Teknik Informatika',
+                            'Perawat' => 'Perawat',
+                            'Hukum' => 'Hukum',
+                        ])
+                    ])
+                    ->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nim')->sortable()->searchable(),
+                TextColumn::make('nama'),
+                TextColumn::make('email'),
+                TextColumn::make('prodi'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMahasiswas::route('/'),
+            'create' => Pages\CreateMahasiswa::route('/create'),
+            'edit' => Pages\EditMahasiswa::route('/{record}/edit'),
+        ];
+    }
+}
